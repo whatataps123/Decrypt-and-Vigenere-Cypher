329d54fed45d54d9769091a2cdf376ef085d31af
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-04-17 07:00:11 +0800

    user input, change character, and print the output

--- decryption.py
+++ decryption.py
@@ -1,9 +1,18 @@
 # Joshua Lemuel Z. Centina BS CpE 1-4
 # ask user to input encrypted text and save it
+text_input = input("Enter a string to decrypt: ")
+text_output = ""
 # check every character
+for i in range(len(text_input)):
 # if * then change a
+    if text_input[i] == "*":
+        text_output += "a"
 # if & then change e
 # if # then change i
 # if + then change o
 # if ! then change u
+
+    else:
+        text_output += text_input[i]
 # print output
+print(text_output)
