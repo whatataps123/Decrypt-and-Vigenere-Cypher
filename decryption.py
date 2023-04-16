edb7fd342d2f47c6e61dd788c485b33c0933b46f
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-04-17 06:52:41 +0800

    pseudocode

---
+++ decryption.py
@@ -0,0 +1,9 @@
+# Joshua Lemuel Z. Centina BS CpE 1-4
+# ask user to input encrypted text and save it
+# check every character
+# if * then change a
+# if & then change e
+# if # then change i
+# if + then change o
+# if ! then change u
+# print output
