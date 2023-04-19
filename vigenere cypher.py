1692855920a6d58998f53d41032df7ff58329454
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-04-19 11:58:24 +0800

    pseudocode

---
+++ vigenere cypher.py
@@ -0,0 +1,12 @@
+#Joshua Lemuel Z. Centina BS CPE 1-4 Vigenere Cypher
+# store the 26 letters of the alphabet in uppercase
+# ask for the user to input a message to be encrypted and save it
+# check message and convert to uppercase
+# ask for the user to input a key for encryption and save it
+# check the key and convert to uppercase
+# convert letters to numbers
+# initialize an empty list to store encrypted indices
+# check the index
+# convert numbers back to letters
+# convert the list of encrypted back to letters
+# print output 
